192a05ea4cadf82c3af9825e8e74d69a0c828872
Divine <dpkreativ@gmail.com>
2022-02-03 17:14:00 +0100

    fetch data using Nuxt's asyncData

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,15 @@
 <script>
 export default {
   name: 'IndexPage',
+  async asyncData({ $content }) {
+    const posts = await $content()
+      .only(['title', 'thumbnail', 'tags', 'slug'])
+      .sortBy('createdAt', 'desc')
+      .fetch()
+    console.log('posts', posts)
+    return {
+      posts,
+    }
+  },
 }
 </script>
