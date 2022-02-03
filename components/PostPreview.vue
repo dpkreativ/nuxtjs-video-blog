028a58684d85731d48e12624954bbfda32f5ace6
Divine <dpkreativ@gmail.com>
2022-02-03 17:15:57 +0100

    create PostPreview component

---
+++ components/PostPreview.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <a :href="`/blog/${post.slug}`">
+      <div class="w-full max-w-sm h-64 overflow-hidden rounded-lg">
+        <img
+          :src="post.thumbnail"
+          alt="blogpost preview"
+          class="object-cover w-full h-full"
+        />
+      </div>
+      <ul v-if="post.tags" class="flex space-x-3 mt-2">
+        <li v-for="tag in post.tags" :key="tag">{{ tag }}</li>
+      </ul>
+      <h2 class="font-semibold text-xl mt-2">{{ post.title }}</h2>
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    post: Object,
+  },
+}
+</script>
