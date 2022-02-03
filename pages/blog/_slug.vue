e8f9c9d6cb59156e71d8e1c03f0252fafe981016
Divine <dpkreativ@gmail.com>
2022-02-03 17:45:32 +0100

    update icon color

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -11,7 +11,7 @@
         >
           <path
             d="M5.83 11L8.41 8.41L7 7L2 12L7 17L8.41 15.59L5.83 13H22V11H5.83Z"
-            fill="#2E3A59"
+            fill="currentColor"
           />
         </svg>
 
