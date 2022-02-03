775f37e3d3d71ef7f94e999b48aa7fd321629247
Divine <dpkreativ@gmail.com>
2022-02-03 17:18:02 +0100

    create dynamic post routes

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>{{ post.title }}</h1>
+</template>
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const post = await $content(params.slug).fetch()
+    return { post }
+  },
+}
+</script>
